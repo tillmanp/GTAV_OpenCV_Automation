15cb0eeedea824d642fa7d58a0908ee8d195f982
Peter Tillman <tillmanp50@gmail.com>
2018-04-19 13:34:29 -0700

    Add files via upload

---
+++ screengrab.py
@@ -0,0 +1,18 @@
+from PIL import ImageGrab
+import numpy as np
+import cv2
+import time
+
+last_time = time.time()
+
+while(True):
+    img = ImageGrab.grab(bbox=(10,40,1920,1120)) #bbox specifies specific region (bbox= x,y,width,height)
+    img_np = np.array(img)
+    img = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+
+    print('loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
+    
+    cv2.imshow('GTAV_OpenCV_Output', np.array(img))
+    cv2.waitKey(25) & 0xFF == ord('q')
+cv2.destroyAllWindows()
