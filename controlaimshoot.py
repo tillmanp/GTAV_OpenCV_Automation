07a4fc7e24e0c2f4076261674009f46ddc1d85d3
Peter Tillman <tillmanp50@gmail.com>
2018-07-01 08:47:06 -0700

    Add files via upload

---
+++ controlaimshoot.py
@@ -0,0 +1,77 @@
+import ctypes
+import time
+import pyautogui
+
+
+SendInput = ctypes.windll.user32.SendInput
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# directx scan codes
+
+if __name__ == '__main__':
+    while (True):
+        PressKey(0x0A)
+        time.sleep(1)
+        ReleaseKey(0x0A)
+        time.sleep(.2)
+
+ #Mouse/Aim
+        time.sleep(4) 
+ 
+        # makes program execution pause for 10 sec
+        pyautogui.moveTo(1000, 1000, duration = 1) 
+ 
+        # moves mouse to 1000, 1000.
+        pyautogui.dragRel(100, 0, duration = 1)
+ 
+        # drags mouse 100, 0 relative to its previous position, 
+        # thus dragging it to 1100, 1000
+        pyautogui.dragRel(0, 100, duration = 1)
+        pyautogui.dragRel(-100, 0, duration = 1)
+        pyautogui.dragRel(0, -100, duration = 1)
